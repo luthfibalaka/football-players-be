2dc47a033679b213130aeeefda51311d9355fbae
luthfibalaka <luthfibalaka@gmail.com>
2023-12-03 05:28:56 +0700

    Setup for vercel deployment

--- football_players/settings.py
+++ football_players/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b&sz1*d)9e_!#5)d7zo7!lc)kio)thjr54digv-)xe%s_)6fdq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
